175e618729f3f2537eb0e3557874a073c1457926
Sonu <sonukn089@gmail.com>
2018-05-27 00:20:52 +0530

    add something

--- admin.php
+++ admin.php
@@ -2,11 +2,54 @@
 <html>
 <head>
 	<title>Admin Page</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./bootstrap.min.css"> <!-- CSS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script><!--Jquery-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><!--bootstrap-->
 </head>
 <body>
 	<center>
 		<h1 style="background-color: blue; color: white;">Welcome to Admin Panel</h1>
 	</center>
-	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-2" style="background-color: gray;">
+				<div class="panel-group">
+			    <div class="panel panel-default">
+			      	<div class="panel-heading">
+			        	<h4 class="panel-title">
+			          		<a data-toggle="collapse" href="#collapse1">Collapsible list group</a>
+			        	</h4>
+			      	</div>
+			      	<div id="collapse1" class="panel-collapse collapse">
+			        	<ul class="list-group">
+			          		<li class="list-group-item">One</li>
+			          		<li class="list-group-item">Two</li>
+			          <li class="list-group-item">Three</li>
+			        </ul>
+			        <div class="panel-footer">Footer</div>
+			      </div>
+			    </div>
+			  </div>
+
+
+
+
+				<ul>
+					<li>Sonu</li>
+					<li>Monu</li>
+					<li>KKK</li>
+					<li>PPPP</li>
+				</ul>
+			</div>
+			<div class="col-sm-10">
+				<h3>Column 2</h3>	
+				<p>Lorem ipsum dolor..</p>
+				<p>Ut enim ad..</p>
+			</div>
+		</div>
+	</div>
+
 </body>
 </html>
