e8f18a3b8d284abaa67ad4d20c3e81cdb3d5993d
Sonu <sonukn089@gmail.com>
2018-05-26 23:49:30 +0530

    update all files

---
+++ admin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Page</title>
+</head>
+<body>
+	<center>
+		<h1 style="background-color: blue; color: white;">Welcome to Admin Panel</h1>
+	</center>
+	
+</body>
+</html>
