16bf94322e4f4760ee6d93e59e25aa75369aa62b
Sonu <sonukn089@gmail.com>
2018-05-27 00:21:24 +0530

    add something

--- admin.php
+++ admin.php
@@ -15,27 +15,6 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-2" style="background-color: gray;">
-				<div class="panel-group">
-			    <div class="panel panel-default">
-			      	<div class="panel-heading">
-			        	<h4 class="panel-title">
-			          		<a data-toggle="collapse" href="#collapse1">Collapsible list group</a>
-			        	</h4>
-			      	</div>
-			      	<div id="collapse1" class="panel-collapse collapse">
-			        	<ul class="list-group">
-			          		<li class="list-group-item">One</li>
-			          		<li class="list-group-item">Two</li>
-			          <li class="list-group-item">Three</li>
-			        </ul>
-			        <div class="panel-footer">Footer</div>
-			      </div>
-			    </div>
-			  </div>
-
-
-
-
 				<ul>
 					<li>Sonu</li>
 					<li>Monu</li>
