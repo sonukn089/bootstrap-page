999d1532cd587e356453aeb50fda6f3237024c10
Sonu <sonukn089@gmail.com>
2018-05-28 22:56:47 +0530

    add something

--- admin.php
+++ admin.php
@@ -15,20 +15,12 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-2">
-                <div class="bg-success">
-                	<div>Admin</div>
-                	<div>Student</div>
-                	<div>Teacher</div>
-                	<div>Librarian</div>
-                	<div>Accountant</div>
-                	<div>Parent</div>
-                </div>
 
                 <div class="panel-group">
   					<div class="panel panel-default">
 					    <div class="panel-heading">
 					      <h4 class="panel-title">
-					        <a data-toggle="collapse" href="#collapse1">Collapsible List</a>
+					        <a data-toggle="collapse" href="#collapse1">Admin</a>
 					      </h4>
 					    </div>
 					    <div id="collapse1" class="panel-collapse collapse">
@@ -45,10 +37,10 @@
   					<div class="panel panel-default">
 					    <div class="panel-heading">
 					      <h4 class="panel-title">
-					        <a data-toggle="collapse" href="#collapse1">Collapsible List</a>
+					        <a data-toggle="collapse" href="#collapse2">Student</a>
 					      </h4>
 					    </div>
-					    <div id="collapse1" class="panel-collapse collapse">
+					    <div id="collapse2" class="panel-collapse collapse">
 					      <ul class="list-group">
 					        <li class="list-group-item">One</li>
 					        <li class="list-group-item">Two</li>
@@ -62,10 +54,10 @@
   					<div class="panel panel-default">
 					    <div class="panel-heading">
 					      <h4 class="panel-title">
-					        <a data-toggle="collapse" href="#collapse1">Collapsible List</a>
+					        <a data-toggle="collapse" href="#collapse3"></a>
 					      </h4>
 					    </div>
-					    <div id="collapse1" class="panel-collapse collapse">
+					    <div id="collapse3" class="panel-collapse collapse">
 					      <ul class="list-group">
 					        <li class="list-group-item">One</li>
 					        <li class="list-group-item">Two</li>
@@ -75,10 +67,27 @@
   					</div>
 				</div>
 
-
-
+				<div class="panel-group">
+  					<div class="panel panel-default">
+					    <div class="panel-heading">
+					      <h4 class="panel-title">
+					        <a data-toggle="collapse" href="#collapse4">Collapsible List</a>
+					      </h4>
+					    </div>
+					    <div id="collapse4" class="panel-collapse collapse">
+					      <ul class="list-group">
+					        <li class="list-group-item">One</li>
+					        <li class="list-group-item">Two</li>
+					        <li class="list-group-item">Three</li>
+					      </ul>
+					    </div>
+  					</div>
+				</div>
 
 			</div>
+
+
+
 			<div class="col-sm-10">
 				<h3>Column 2</h3>	
 				<p>Lorem ipsum dolor..</p>
