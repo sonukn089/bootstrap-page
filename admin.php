0896fe02ea758235ab3582332f45bdc77bb1a65c
Sonu <sonukn089@gmail.com>
2018-05-28 22:58:31 +0530

    add something

--- admin.php
+++ admin.php
@@ -54,7 +54,7 @@
   					<div class="panel panel-default">
 					    <div class="panel-heading">
 					      <h4 class="panel-title">
-					        <a data-toggle="collapse" href="#collapse3"></a>
+					        <a data-toggle="collapse" href="#collapse3">Teacher</a>
 					      </h4>
 					    </div>
 					    <div id="collapse3" class="panel-collapse collapse">
@@ -71,10 +71,27 @@
   					<div class="panel panel-default">
 					    <div class="panel-heading">
 					      <h4 class="panel-title">
-					        <a data-toggle="collapse" href="#collapse4">Collapsible List</a>
+					        <a data-toggle="collapse" href="#collapse4">Librarian</a>
 					      </h4>
 					    </div>
 					    <div id="collapse4" class="panel-collapse collapse">
+					      <ul class="list-group">
+					        <li class="list-group-item">One</li>
+					        <li class="list-group-item">Two</li>
+					        <li class="list-group-item">Three</li>
+					      </ul>
+					    </div>
+  					</div>
+				</div>
+
+				<div class="panel-group">
+  					<div class="panel panel-default">
+					    <div class="panel-heading">
+					      <h4 class="panel-title">
+					        <a data-toggle="collapse" href="#collapse5">Accountant</a>
+					      </h4>
+					    </div>
+					    <div id="collapse5" class="panel-collapse collapse">
 					      <ul class="list-group">
 					        <li class="list-group-item">One</li>
 					        <li class="list-group-item">Two</li>
