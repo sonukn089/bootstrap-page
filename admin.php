06841da69e65819eb0b7d09951e118e0296327a3
Sonu <sonukn089@gmail.com>
2018-05-28 22:30:25 +0530

    add something

--- admin.php
+++ admin.php
@@ -24,12 +24,35 @@
 	                        </h4>
 	                    </div>
 	                    <div id="collapse1" class="panel-collapse collapse in">
-	                        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-	                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+	                        <div class="panel-body">
+	                        	<div>Sonu</div>
+	                        	<div>Monu</div>
+	                        	<div>Neeraj</div>
+	                        	<div>Pushpendra</div>
 	                        </div>
 	                    </div>
 	                </div>
             	</div>
+            	<div class="panel-group" id="accordion">
+	                <div class="panel panel-default">
+	                    <div class="panel-heading">
+	                        <h4 class="panel-title">
+	                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Collapsible Group 1</a>
+	                        </h4>
+	                    </div>
+	                    <div id="collapse1" class="panel-collapse collapse in">
+	                        <div class="panel-body">
+	                        	<div>Sonu</div>
+	                        	<div>Monu</div>
+	                        	<div>Neeraj</div>
+	                        	<div>Pushpendra</div>
+	                        </div>
+	                    </div>
+	                </div>
+            	</div>
+
+
+            	
 			</div>
 			<div class="col-sm-10">
 				<h3>Column 2</h3>	
